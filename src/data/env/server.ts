8d9ccb2ac78982f7aa0e76d1e5330066e1fe2a99
abdulahad565 <aahad23p@gmail.com>
2025-03-18 17:56:49 +0500

    clerk setup

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+   CLERK_SECRET_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: process.env,
 });
