526086962b6254397db3ce59098aef1c91a14bb0
abdulahad565 <aahad23p@gmail.com>
2025-03-21 16:05:44 +0500

    2:11

--- src/features/courses/actions/courses.ts
+++ src/features/courses/actions/courses.ts
@@ -17,3 +17,14 @@ export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
 
   redirect(`/admin/courses/${course.id}/edit`);
 }
+
+
+export async function deleteCourse(id: string) {
+  if (!canDeleteCourses(await getCurrentUser())) {
+    return { error: true, message: "Error deleting your course" }
+  }
+
+  await deleteCourseDB(id)
+
+  return { error: false, message: "Successfully deleted your course" }
+}
