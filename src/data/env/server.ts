41544ab81639f9b2e4cdaf2d32c30b0486ace4d9
Hassam-Ata <hassamata86@gmail.com>
2025-04-04 03:27:47 +0500

    feat: enhance middleware with arcjet integration and user country header management

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -4,8 +4,10 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-   CLERK_SECRET_KEY: z.string().min(1),
-   CLERK_WEBHOOK_SECRET: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1),
+    CLERK_WEBHOOK_SECRET: z.string().min(1),
+    ARCJET_KEY: z.string().min(1),
+    TEST_IP_ADDRESS: z.string().min(1).optional(),
   },
   experimental__runtimeEnv: process.env,
 });
